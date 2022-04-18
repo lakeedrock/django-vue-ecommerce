2b4244606d248d6a165bfa806da55bbffb0c23f4
Lakshan Costa <lakeedrock@gmail.com>
2022-04-18 17:59:13 +0300

    Display recent products

--- jackets_vue/src/views/HomeView.vue
+++ jackets_vue/src/views/HomeView.vue
@@ -1,13 +1,77 @@
 <template>
-  <div class="home">Home</div>
+  <div class="home">
+    <section class="hero is-medium is-dark mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">Welcome to Jacket Store</p>
+        <p class="subtitle">The best jacket store online</p>
+      </div>
+    </section>
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered">Latest Products</h2>
+      </div>
+      <div
+        class="column is-3"
+        v-for="product in latestProducts"
+        v-bind:key="product.id"
+      >
+        <div class="box">
+          <figure class="image mb-4">
+            <img v-bind:src="product.get_thumbnail" alt="" />
+          </figure>
+          <h3 class="is-size-4">
+            {{ product.name }}
+          </h3>
+          <p class="is-size-6 has-text-grey">
+            <i class="fas fa-dollar-sign"></i>
+            {{ product.price }}
+          </p>
+          View Details
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
   name: "HomeView",
+  data() {
+    return {
+      latestProducts: [],
+    };
+  },
   components: {},
+  mounted() {
+    this.getLatestProducts();
+  },
+  methods: {
+    getLatestProducts() {
+      axios
+        .get("/api/v1/latest-products")
+        .then((response) => {
+          console.log(response.data);
+          this.latestProducts = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
 };
 </script>
+
+<style scoped>
+.image {
+  margin-top: -1.25rem;
+  margin-left: -1.25rem;
+  margin-right: -1.25rem;
+}
+
+.image img {
+  height: 350px;
+}
+</style>
