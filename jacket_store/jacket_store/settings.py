71514eda8b7aa72e5b372a3788ef25a3e5b8a571
Lakshan Costa <lakeedrock@gmail.com>
2022-04-18 15:55:42 +0300

    Creating products, categories and recent products api response

--- jacket_store/jacket_store/settings.py
+++ jacket_store/jacket_store/settings.py
@@ -37,10 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
     'djoser',
+
+    'product',
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -127,3 +130,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
